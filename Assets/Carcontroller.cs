4ab6b6da19a2512fa93df3099eaac1d6adc0067e
shariful islam <sisagor98@gmail.com>
2019-11-20 00:47:59 +0600

    Replika

---
+++ Assets/Carcontroller.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carcontroller : MonoBehaviour
+{
+    bool move = false;
+    bool isGrounded= false;
+    public float rotationSpeed = 2f;
+
+    public float speed = 500f;
+    public Rigidbody2D rigidbody;
+
+    void Start() {
+        rigidbody = GetComponent<Rigidbody2D>();    
+    }
+
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1")){
+            move = true;
+        }
+        if(Input.GetButtonUp("Fire1")){
+            move = false;
+        }
+    }
+
+    private void FixedUpdate() {
+        if(move == true) 
+        {
+            if (isGrounded){
+                rigidbody.AddForce(transform.right * speed * Time.fixedDeltaTime * 100f, ForceMode2D.Force);
+            }
+            else{
+                rigidbody.AddTorque(rotationSpeed * Time.fixedDeltaTime * 100f, ForceMode2D.Force);
+            }
+
+        }   
+    }
+
+    private void OnCollisionEnter2D()
+	{
+		isGrounded = true;
+	}
+
+	private void OnCollisionExit2D()
+	{
+		isGrounded = false;
+	}
+
+
+
+}
